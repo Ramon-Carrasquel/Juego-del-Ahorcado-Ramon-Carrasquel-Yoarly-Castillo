e5f92771d671e5ce9441f0e4078e947d4809ec17
Ramon-Carrasquel <ramon.carrasquel@correo.unimet.edu.ve>
2024-05-24 18:09:04 -0400

    initial commit

---
+++ scripts/listapalabras.js
@@ -0,0 +1,50 @@
+const listaPalabras = [
+    {
+        palabra: "colegio",
+        pista: "Institucion para niños"
+    },
+    {
+        palabra: "sistema",
+        pista: "conjunto de elementos interrelacionados e interdependientes"
+    },
+    {
+        palabra: "software",
+        pista: "sistema formal de un sistema informático"
+    },
+    {
+        palabra: "hardware",
+        pista: "partes tangibles de un sistema informático"
+    },
+    {
+        palabra: "modelo",
+        pista: "abstraccion del mundo real"
+    },
+    {
+        palabra: "uml",
+        pista: "lenguaje de modelado visual"
+    },
+    {
+        palabra: "diagrama",
+        pista: "diseños geométricos para representar gráficamente ideas"
+    },
+    {
+        palabra: "clase",
+        pista: "clasificador de objetos"
+    },
+    {
+        palabra: "html",
+        pista: "lenguaje para la estructura de páginas"
+    },
+    {
+        palabra: "css",
+        pista: "lenguaje del diseño"
+    },
+    {
+        palabra: "javascript",
+        pista: "crea contenido de actualización dinámica"
+    },
+    {
+        palabra: "diseño",
+        pista: "configuración mental antes de la búsqueda de soluciones"
+    }
+]
---
+++ scripts/script.js
@@ -0,0 +1,81 @@
+const imagenHorca = document.querySelector(".caja-horca img");
+const displayPalabras = document.querySelector(".display-palabras");
+const textoIntentos = document.querySelector(".texto-intentos b");
+const tecladoDiv = document.querySelector(".teclado");
+const cuadrofinJuego = document.querySelector(".cuadro-finjuego");
+const jugardeNuevo = document.querySelector(".cuadro-finjuego");
+
+let palabraActual, letrasCorrectas, intentosFallidosContador;
+const maxIntentos = 6;
+
+const resetearJuego = () => {
+  //Resetea el juego, sus variables, y los elementos visuales
+  letrasCorrectas = [];
+  intentosFallidosContador = 0;
+  imagenHorca.src = `imagenesdeapoyo/hangman-${intentosFallidosContador}.svg`;
+  textoIntentos.innerText = `${intentosFallidosContador} / ${maxIntentos}`;
+  tecladoDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+  displayPalabras.innerHTML = palabraActual.split("").map(() => `<li class="letra"></li>`).join("");
+  cuadrofinJuego.classList.remove("show");
+}
+
+const obtenPalabraCualquiera = () => {
+  //Selecciona una palabra cualquiera junto a su pista, de la lista de palabras
+  const { palabra, pista } = listaPalabras[Math.floor(Math.random() * listaPalabras.length)];
+  palabraActual = palabra;
+  document.querySelector(".texto-pista b").innerText = pista;
+  resetearJuego();
+}
+
+const findeJuego = (victoria) => {
+  //Muestra un cuadro con la informacion luego de haber acabado el juego, o luego de 600ms
+  setTimeout(() => {
+    const cajaTexto = victoria ? `Adivinaste la palabra: ` : `La palabra correcta es: `;
+    cuadrofinJuego.querySelector("h4").innerText = `${victoria ? `¡Ganaste!` : `¡Perdiste`}`;
+    cuadrofinJuego.querySelector("p").innerHTML = `${cajaTexto} <b>${palabraActual}</b>`;
+    cuadrofinJuego.classList.add("show");
+  }, 300);
+}
+
+const iniciarJuego = (button, letraClickeada) => {
+  //Verifica si la letra que fue clickeada se encuentra en la palabra actual
+  if (palabraActual.includes(letraClickeada)){
+    //Muestra todas las letras correctas que fueron seleccionadas
+    [...palabraActual].forEach((letra, index) => {
+      if (letra === letraClickeada){
+        letrasCorrectas.push(letra);
+        displayPalabras.querySelectorAll("li")[index].innerText = letra;
+        displayPalabras.querySelectorAll("li")[index].classList.add("adivinada");
+      }
+    });
+  }else{
+    //Si la letra elegida no esta en la palabra, se actualiza el contador de intentos y la imagen de la horca
+    intentosFallidosContador++;
+    imagenHorca.src = `imagenesdeapoyo/hangman-${intentosFallidosContador}.svg`;
+  }
+  button.disabled = true;
+  textoIntentos.innerText = `${intentosFallidosContador} / ${maxIntentos}`;
+
+  //Llamamos la funcion de fin de juego en caso de que se cumpla uno de los condicionales
+  if (intentosFallidosContador === maxIntentos) return findeJuego(false);
+  if (letrasCorrectas.length === palabraActual.length) return findeJuego(true);
+}
+
+//Bucle for para los botones del teclado y para los addEventListener
+for (let i = 97; i <= 122; i++) {
+  const button = document.createElement("button");
+  button.innerText = String.fromCharCode(i);
+  tecladoDiv.appendChild(button);
+  button.addEventListener("click", e => iniciarJuego(e.target, String.fromCharCode(i)));
+
+  // Añadir la letra ñ entre n y o
+  if (i === 110) {
+    const buttonEnye = document.createElement("button");
+    buttonEnye.innerText = "ñ";
+    tecladoDiv.appendChild(buttonEnye);
+    buttonEnye.addEventListener("click", e => iniciarJuego(e.target, "ñ"));
+  }
+}
+
+obtenPalabraCualquiera();
+jugardeNuevo.addEventListener("click", obtenPalabraCualquiera)
